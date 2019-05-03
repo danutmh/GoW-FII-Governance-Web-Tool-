ecc7c09bcbb962d601bb13923a4a712d56f5feef
cosminm13 <48436357+cosminm13@users.noreply.github.com>
2019-05-03 20:07:40 +0300

    updated page with project description

--- gow2/about.php
+++ gow2/about.php
@@ -10,14 +10,11 @@
 
 <div class="container" id="main-content">
 	<h2>About Us</h2>
-	<p>"About Us" conten goes here.  I'll stick with teh "lorem ipsum" as well, so that the footer isn't immediately following this text.</p>
-
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
+	<p>Activitatea Facultatii de Informatica este guvernata de diverse acte legislative (i.e. legi si prevederi). </p>
+    <p>Acestea pot avea caracter: general (e.g. legi nationale, hotarari/ordonante guvernamentale etc.), local (i.e. pe teritoriul judetului/municipiului Iasi, hotarari ale Consiliului Municipal sau ale Primariei), universitar (de pilda, hotarari ale Senatului UAIC) si specifice FII (uzual, prevederi ale Consiliului facultatii -- de exemplu, referitoare la acordarea burselor ori la organizarea admiterii). </p>
+    <p>De remarcat faptul ca unele documente legislative, oferite in formatele HTML si PDF, nu vor putea fi accesate decat de anumite categorii de utilizatori: profesori titulari, colaboratori, studenti (inmatriculati la studii de licenta, masterat sau doctorat), personal auxiliar, externi. Pot exista cazuri in care reglementarile interne FII se refera exclusiv la cadrele didactice ori doar la categorii specifice de studenti.</p>
+    <p>Aplicatia Web implementata va oferi informatii de interes despre toate aceste legi si prevederi (inclusiv privind data promulgarii, audienta, cuvinte-cheie, valabilitate etc.) cu posibilitatea cautarii avansate si a restrictionarii accesului -- de exemplu, persoanele din afara FII nu au dreptul sa consulte anumite regulamente interne sau conducerea facultatii (decan, prodecani, sef de departament) va avea drepturi de editare/adnotare a unei propuneri de regulament ce va fi ulterior dezbatuta si, eventual, aprobata de Consiliul FII.</p>
+    <p>Noutatile vizand noile reglementari ori initiative legislative de interes vor fi expuse si via un flux de stiri Atom.</p>
 </div>
 
 <?php include("includes/footer.php");?>
